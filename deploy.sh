eb7aa01724dce240d47f29fb02bc773b995b14a1
Angel Nuñez <snahider@gmail.com>
2020-12-24 17:16:51 -0500

    change context

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TAG_NAME=katu-portal-cache
+docker build -t $TAG_NAME /home/ec2-user/katu-portal
+
+for runName in `docker ps | grep "$TAG_NAME" | awk '{print $1}'`
+do
+    if [ "$runName" != "" ]
+    then
+        docker stop $runName
+    fi
+done
+docker run --name $TAG_NAME --rm -d -p 80:3000 $TAG_NAME
---
+++ server.js
@@ -0,0 +1,55 @@
+'use strict';
+const express = require('express');
+var child_process = require('child_process');
+
+function run_script(command, callback) {
+  console.log("Starting Process.");
+  var child = child_process.spawn(command);
+
+  var scriptOutput = "";
+
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', function(data) {
+      console.log('stdout: ' + data);
+
+      data=data.toString();
+      scriptOutput+=data;
+  });
+
+  child.stderr.setEncoding('utf8');
+  child.stderr.on('data', function(data) {
+      console.log('stderr: ' + data);
+
+      data=data.toString();
+      scriptOutput+=data;
+  });
+
+  child.on('close', function(code) {
+      callback(scriptOutput,code);
+  });
+}
+
+
+
+const app = express();
+var appState="App is Running"
+
+app.get('/', (req, res) => {
+  res.send(`App State: ${appState}`);
+});
+
+app.get('/deploy', (req, res) => {
+  appState="Deploy in Progress";
+  run_script("./deploy.sj", function(output, exit_code) {
+    appState="Finish Deployment. App is Running";
+    console.log("Deploy Finished.");
+    console.log('Exit Code: ' + exit_code);
+    console.log('Full output of script: ',output);
+  });
+  res.send('Deploying new Version. Check new version in meta.json');
+});
+
+const PORT = 9090;
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
