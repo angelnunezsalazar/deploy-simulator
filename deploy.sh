37dfc151f66752d6a4590a12ca8f1711555d2595
Angel Nuñez <snahider@gmail.com>
2020-12-24 17:47:13 -0500

    use touch

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 TAG_NAME=katu-portal-cache
 
 echo "Force Building container (don't use cache) ..."
-put /home/ec2-user/katu-portal/force-build.txt
+touch /home/ec2-user/katu-portal/force-build.txt
 
 echo "Building Container ..."
 
